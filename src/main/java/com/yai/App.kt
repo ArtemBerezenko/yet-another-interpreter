e7df20276252cc58d8730d1ea6d8e32ab5809289
ArtemBerezenko <a.a.berezenko@gmail.com>
2023-03-12 16:17:30 +0300

    init commit

---
+++ src/main/java/com/yai/App.kt
@@ -0,0 +1,6 @@
+package com.yai
+
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/test/java/com/yai/AppTest.kt
@@ -0,0 +1,11 @@
+package com.yai
+
+import org.junit.Assert
+import org.junit.Test
+
+class AppTest {
+    @Test
+    fun shouldAnswerWithTrue() {
+        Assert.assertTrue(true)
+    }
+}
